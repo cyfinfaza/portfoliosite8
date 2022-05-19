3b3e5f209217fa5a27bb78fba5ba9c59643e6454
cyfinfaza <cyfinfaza@gmail.com>
2022-05-19 09:32:09 -0400

    automatically compile sites.json on server start

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+import yaml from 'yaml';
+import fs from 'fs';
+
+console.log('Converting sites.yml to sites.json');
+const sites = yaml.parse(fs.readFileSync('./sites.yml', 'utf8'));
+// create temp directory if it does not exits
+if (!fs.existsSync('./src/data')) fs.mkdirSync('./src/data');
+fs.writeFileSync('./src/data/sites.json', JSON.stringify(sites));
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
